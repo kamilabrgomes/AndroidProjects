7240879bef9640703aa2c8a308bfe4557dfe8975
kamila.gomes <kamila.br.gomes@hotmail.com>
2023-06-22 10:03:47 -0300

    Projeto de criação de interface de app de notícias.

---
+++ ConstraintLayout/app/src/main/java/com/cursoandroid/constraintlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cursoandroid.constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//Criação de uma interface de app de notícias - exercício prático de layouts
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
